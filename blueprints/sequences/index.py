ab87ecb3ee6ca5cf9682b5251ee9dc309a7ab018
cschulzk <christophschulzke@gmail.com>
2023-07-09 22:52:08 -0500

    Move functions into subroutes under their resprective branch of science

---
+++ blueprints/sequences/index.py
@@ -0,0 +1,9 @@
+from flask import Blueprint, jsonify
+from blueprints.sequences.collatz import collatz_bp
+sequences_bp = Blueprint('sequences', __name__, url_prefix='/sequences')
+sequences_bp.register_blueprint(collatz_bp)
+
+@sequences_bp.route('/', methods=['GET'])
+def sequences():
+    return jsonify({"help": "This is how to use this route."})
+    
--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,10 @@
 from flask import Flask
-from blueprints.collatz import collatz_bp
-from blueprints.exponentiate import exponentiate_bp
-
+from blueprints.arithmetic.index import arithmetic_bp
+from blueprints.sequences.index import sequences_bp
 app = Flask(__name__, instance_relative_config=True)
 
-app.register_blueprint(exponentiate_bp)
-app.register_blueprint(collatz_bp)
+app.register_blueprint(sequences_bp)
+app.register_blueprint(arithmetic_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
